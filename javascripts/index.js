93ae83681190c6e6705864778417ad02fa1c6607
CTIHVKE <ctihvke@outlook.com>
2016-06-07 09:31:29 +0800

    two
    20160607

---
+++ javascripts/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ctihv on 2016/6/7.
+ */
+
+$(document).ready(function(){
+
+    function bt_run(){
+        $("#bt_left").click(
+            function () {
+
+            }
+        );
+        $("#bt_right").click(
+            function () {
+
+            }
+        );
+    }
+});
