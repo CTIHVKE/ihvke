6ef33bc382b47b1d67f540b20c0dae65dc399880
CTIHVKE <ctihvke@outlook.com>
2016-06-07 10:54:18 +0800

    js_run

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,16 +3,16 @@
  */
 
 $(document).ready(function(){
-
+    bt_run();
     function bt_run(){
         $("#bt_left").click(
             function () {
-
+                $("#img_ul").animate({left:"2"});
             }
         );
         $("#bt_right").click(
             function () {
-
+                $("#img_ul").animate({left:"-408"});
             }
         );
     }
