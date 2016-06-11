93633d734a86a160ba865964348ea3d4760ef34c
CTIHVKE <ctihvke@outlook.com>
2016-06-11 10:07:42 +0800

    NEW20160611

---
+++ javascripts/canvas.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ctihv on 2016/6/9.
+ */
+
+
+function click(){
+    document.getElementById("li_a").onclick=function(){
+
+    }
+    document.getElementById("li_b").onclick=function(){
+
+    }
+    document.getElementById("li_c").onclick=function(){
+
+    }
+    document.getElementById("li_d").onclick=function(){
+
+    }
+    document.getElementById("li_e").onclick=function(){
+
+    }
+    document.getElementById("li_f").onclick=function(){
+
+    }
+    document.getElementById("li_g").onclick=function(){
+
+    }
+    document.getElementById("li_h").onclick=function(){
+
+    }
+}
+
+
+function run(){
+    if(!document.getElementById) return false;
+    //click();
+}
