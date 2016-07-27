0a161af05ffc36f64b6baa438476590aaf863ca4
CTIHVKE <ctihvke@outlook.com>
2016-07-27 12:26:16 +0800

    rsjc

--- rsjc/rsjc.js
+++ rsjc/rsjc.js
@@ -2,16 +2,25 @@
  * Created by CTIHVKE on 2016/7/26.
  */
 
-var btn_stat = document.getElementById("stat");
 
-var canvar = document.getElementById("canvasjg");
-var cc = canvar.getContext("2d");
+var canid = document.getElementById("canid");
+
+var canvar;     //canvasjg 对象
+var cc;         //canvar。getContext("2d");
+
 var name = "";
 var sex = "";
 var y = "";
 var m = "";
 var d = "";
-var str = new Array["111","222","333","444","555","666","777","888","999","000","121212","131313"];
+var str = new Array()["111","222","333","444","555","666","777","888","999","000","121212","131313"];
+
+
+var canvarstar = function(){
+    canvar = document.getElementById("canvasjg");
+    cc = canvar.getContext("2d");
+}
+
 
 var err = function(){
     canvar.log("sth strong");
@@ -20,10 +29,37 @@ var rd = function(i){
     return Math.random()*i
 }
 
-var gettext = function(){
-    name = document.getElementById("inname");
-    sex = document.getElementById("insex");
-    iny = document.getElementById("iny");
-    inm = document.getElementById("inm");
-    ind = document.getElementById("ind");
+var gettext = function(){       //取值
+    name = document.getElementById("inname").value;
+    sex = document.getElementById("insex").value;
+    iny =parseInt( document.getElementById("iny").value);
+    inm =parseInt( document.getElementById("inm").value);
+    ind =parseInt( document.getElementById("ind").value);
 }
+
+//   =========给画======
+var dw1 = function(stri){       //画线
+    cc.font="20px Arial";
+    cc.strokeStyle= "#339933";
+    cc.strokeText(str[stri],10,50);
+}
+
+var dw2 = function(stri){       //填充
+    cc.font="20px Arial";
+    cc.fillStyle = "#993399";
+    cc.fillText(str[stri],20,70);
+}
+
+//======控制=====
+document.getElementById("stat").addEventListener("click",function(){
+    gettext();
+
+    if(name == "" || sex == "" || iny == "" || inm == "" || ind == ""){
+        alert("请完整输入信息");
+        return;
+    }
+
+
+})
+
+//======控制=====
